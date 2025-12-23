49517fc25561fd1b44d622985c2524da42468db5
Ashutosh Singh <ashutosh.singh@inferworks.com>
2025-12-24 00:30:37 +0530

    q41

---
+++ lambda/vairableDemo.js
@@ -0,0 +1,22 @@
+var marks = 100;
+var name = "Ashutosh Singh";
+var age;
+var company = null;
+var salary = undefined;
+var isStudent = false;
+
+console.log("Marks =", marks);
+console.log("Name =", name);
+console.log("Age =", age);
+console.log("Company =", company);
+console.log("Salary =", salary);
+console.log("isStudent =", false);
+
+
+var user={
+name:"AShutoshaa Singh",
+Comapny :"Google",
+age:29 
+}
+
+console.log("Details of the user is ",user);
